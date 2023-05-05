940cfa826e1b9f8c04cdf8caa5447677ba5b0ba2
Mykola Vorotynskyy <vorotynskyy@gmail.com>
2023-05-06 00:48:53 +0200

    commit

---
+++ tenzies/src/App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import './App.css';
+import Die from "./Die";
+
+function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+  function allNewDice() {
+    const newDice = []
+      for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+    }
+   const diceElements = dice.map(die => <Die value={die} />  )
+  
+  return (
+    <div className="App">
+      <main>
+        <div className="dice-container">
+       {diceElements}
+       
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
